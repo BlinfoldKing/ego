e71a00da8a113a160c2e52798150e7e3ac6caeb4
Ganesha Danu <ganeshadanu1@gmail.com>
2019-12-30 07:15:11 +0700

    implement disqus thread

--- src/pages/story/[slug].js
+++ src/pages/story/[slug].js
@@ -1,6 +1,7 @@
 // @flow
 import ReactMarkdown from 'react-markdown';
 import React, { useState, useEffect, useCallback } from 'react';
+import Script from 'react-inline-script';
 import { useLocalForm, useWatchFormValues } from 'tinacms';
 import { useDropzone } from 'react-dropzone';
 import { gql, ApolloClient } from 'apollo-boost';
@@ -254,7 +255,7 @@ export default function Page(props: Props) {
         }
 
       </div>
-      {/* <div className="separator"></div> */}
+
       <div className="container post-navigator">
         <div className="prev">
           {post.prev && (
@@ -281,6 +282,31 @@ export default function Page(props: Props) {
           )}
         </div>
       </div>
+
+      <div>
+        <div id="disqus_thread" className="container"></div>
+      </div>
+      <Script>
+        {`
+            /**
+            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+            /*
+            var disqus_config = function () {
+              this.page.url = 'blinfoldking.dev';  // Replace PAGE_URL with your page's canonical URL variable
+              this.page.identifier = '${post.slug}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+            };
+            */
+            (function() { // DON'T EDIT BELOW THIS LINE
+              var d = document, s = d.createElement('script');
+              s.src = 'https://blinfoldking.disqus.com/embed.js';
+              s.setAttribute('data-timestamp', +new Date());
+              (d.head || d.body).appendChild(s);
+            })();
+        `}
+      </Script>
+      <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
       <div className="top">
         <a href="#">
           <i data-eva="arrowhead-up" />
@@ -334,7 +360,7 @@ export default function Page(props: Props) {
                 .post-navigator {
                     display: flex;
                     padding: 30px 10vw;
-                    border-top: dashed 1px black;
+                   /* border-top: dashed 1px black; */
                     justify-content: space-between;
                 }
 
@@ -394,6 +420,10 @@ export default function Page(props: Props) {
                   justify-content: center;
                   margin-top: 50px;
                 } 
+
+                #disqus_thread {
+                  padding: 0 10vw;
+                }
             `}</style>
     </Layout>
   );
