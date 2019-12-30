062a7426c16722fa7348e6a749811103c37f80db
Ganesha Danu <ganeshadanu1@gmail.com>
2019-12-30 07:33:03 +0700

    fix identifier

--- src/pages/story/[slug].js
+++ src/pages/story/[slug].js
@@ -291,12 +291,13 @@ export default function Page(props: Props) {
             /**
             *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
             *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-            /*
+            */
+
             var disqus_config = function () {
-              this.page.url = 'blinfoldking.dev';  // Replace PAGE_URL with your page's canonical URL variable
+              this.page.url = 'https://www.blinfoldking.dev/story/${post.slug}';  // Replace PAGE_URL with your page's canonical URL variable
               this.page.identifier = '${post.slug}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
             };
-            */
+
             (function() { // DON'T EDIT BELOW THIS LINE
               var d = document, s = d.createElement('script');
               s.src = 'https://blinfoldking.disqus.com/embed.js';
